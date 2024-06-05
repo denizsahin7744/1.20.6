5fcd37cb52b093d281bbc985ec879830a7ad76a0
denizsahin7744 <denizsahin7744@gmail.com>
2024-06-05 11:17:56 +0300

    Ready to use

--- start.sh
+++ start.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env sh
 
-while [ true ]; do
-    java -Xms4096M -Xmx4096M -jar paper.jar nogui
-    echo Server restarting...
-    echo Press CTRL + C to stop.
-done
+#2GB ram
+java -Xms2048M -Xmx2048M -jar paper.jar nogui
+
+#4GB ram
+#java -Xms4096M -Xmx4096M -jar paper.jar nogui
+
+#8GB ram
+#java -Xms8192M -Xmx8192M -jar paper.jar nogui
+
+#12GB ram
+#java -Xms12288M -Xmx12288M -jar paper.jar nogui
+
+#16GB ram
+#java -Xms16384M -Xmx16384M -jar paper.jar nogui
+
+#20GB ram
+#java -Xms20480M -Xmx20480M -jar paper.jar nogui
